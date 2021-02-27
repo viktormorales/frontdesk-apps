3a0821b395b0ee88e57ca611e24cd2237c0ea445
Viktor H. Morales <viktorhugomorales@gmail.com>
2021-02-27 17:45:28 -0300

    New API

--- pages/api/messages/index.js
+++ pages/api/messages/index.js
@@ -1,4 +1,4 @@
-import { connectToDatabase } from "../../../utils/mongodb";
+import { connectToDatabase } from "../../../middleware/mongodb";
 
 export default async (req, res) => {
   const { db } = await connectToDatabase();
