feb3dc212fdee2bbd4120e5f4c212a5800d06ef5
Viktor H. Morales <viktorhugomorales@gmail.com>
2021-02-27 17:44:37 -0300

    Added Material UI Data Grid view

--- pages/admin/guests/index.js
+++ pages/admin/guests/index.js
@@ -1,38 +1,57 @@
 import React, {useState} from 'react'
 import Head from "next/head"
-import { connectToDatabase } from "../../../utils/mongodb"
+import dbConnect from "../../../middleware/mongodb"
+import Guest from '../../../models/Guest'
 import Layout from "../../../layouts/admin/Layout"
 import Container from "@material-ui/core/Container"
 import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
-import { DataGrid } from '@material-ui/data-grid';
+import { DataGrid, useApiRef } from '@material-ui/data-grid';
+import moment from 'moment'
 
-function Guests() {
-	const columns = [
-		{ field: 'suite', headerName: '#', width: 90 },
-		{
-		  field: 'guest',
-		  headerName: 'Cliente',
-		  width: 200,
-		  description: 'This column has a value getter and is not sortable.',
-		  sortable: false,
-		  width: 160,
-		  valueGetter: (params) =>
-			`${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
-		},
-		{ field: 'checkout', headerName: 'Checkout', width: 120 },
-		{ field: 'adults', headerName: 'Adultos', width: 120 },
-	
-	];
+// Esta información la usa el componente data-grid de Material UI
+// para crear la cabecera de la tabla
+const columns = [
+	{ 
+		field: 'checkout_time', 
+		headerName: 'Hora', 
+		width: 90,
+	},
+	{
+		field: 'checkout_date',
+		headerName: 'Checkout'
+	},
+	{ 
+		field: 'suite', 
+		headerName: '#', 
+		width: 90 
+	},
+	{
+		field: 'guest',
+		headerName: 'Cliente',
+		width: 200,
+	},
+	{ 
+		field: 'pax', 
+		headerName: 'PAX', 
+		width: 120 
+	},
+];
 
-	const [guests, setGuest] = useState([]);
+const Guests = (props) => {
 
+	// Los hooks permiten usar el estado y otras características de React sin escribir una clase.
+	// Más información: https://es.reactjs.org/docs/hooks-intro.html
+	const [guests, setGuests] = useState(props.guests);
+	const [dateInput, setDate] = useState(moment().format('YYYY-MM-DD'));
+
+	// Evento que se ejecuta al enviar el formulario de importación
 	const importGuests = async event => {
 		event.preventDefault();
 		
-		// obtiene el archivo
+		// obtiene inputs del formulario
 		let selectedFile = event.target.importCSV.files[0];
 		let selectedDate = event.target.date.value;
 		if (typeof selectedFile === 'undefined')
@@ -41,22 +60,38 @@ function Guests() {
 		if (selectedDate === '')
 			return 'Fecha...'
 			
-		// crea el objeto FormData
+		// crea el objeto FormData con los datos del formulario
 		var formData = new FormData();
 		formData.append('file', selectedFile);
-		formData.append('date', event.target.date.value);
+		formData.append('date', selectedDate);
 		
-		const res = await fetch('/api/guests/import', {
+		// Envia los datos al servidor para ser procesados
+		const res = await fetch('/api/guests', {
 			method: 'POST',
 			body: formData
 		})
 		
+		// Actualiza el listado "guests"
 		if (res.status === 200) { 
 			let response = await res.json();
-			console.log(response.filtered)
-			//setGuest(response.filtered)
+			console.log(response)
+			setGuests(response.filtered)
 		}
 		
+	}
+
+	const guestsByDate = async event => {
+		console.log(event.target.value);
+		await dbConnect()
+
+		const guestsFind = await Guest.find({})
+		const guests = JSON.parse(JSON.stringify(guestsFind))
+		
+		if (!guests) {
+			return {
+				notFound: true,
+			}
+		}
 	}
 
 	return (
@@ -70,7 +105,7 @@ function Guests() {
 				<form autoComplete="off" onSubmit={importGuests} encType="multipart/form-data">
 					<Grid container spacing={3}>
 						<Grid item xs={6}>
-							<TextField id="standard-basic" type="date" name="date" label="" />
+							<TextField id="standard-basic" type="date" name="date" label="" onChange={guestsByDate} defaultValue={dateInput} />
 						</Grid>
 						<Grid item xs={6}>
 							<TextField type="file" name="importCSV" />
@@ -82,7 +117,7 @@ function Guests() {
 				</form>
 
 				<div style={{ height: 400, width: '100%' }}>
-					<DataGrid rows={guests} columns={columns} pageSize={10} checkboxSelection />
+					<DataGrid rows={guests} columns={columns} pageSize={50} checkboxSelection />
 				</div>
 			</Container>
 		</Layout>
@@ -90,13 +125,24 @@ function Guests() {
 	)
 }
 
-// This function gets called at build time
-export async function getServerSideProps() {
-	const { db } = await connectToDatabase();
-	
-	const guests = [];
+export async function getStaticProps(context) {
 
-	return { props: { guests: JSON.parse(JSON.stringify(guests)) } }
+	await dbConnect()
+
+	const guestsFind = await Guest.find({})
+	const guests = JSON.parse(JSON.stringify(guestsFind))
+	
+	if (!guests) {
+		return {
+			notFound: true,
+		}
+	}
+	
+	return {
+		props: {
+			guests
+		},
+	}
 }
 
 export default Guests
